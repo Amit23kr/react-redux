8ee392ec01ced4ffad325a3d2470d3eb13b70c67
Amit23kr <amitsah2393@gmail.com>
2020-07-02 03:45:40 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import App from "./App";
+import "./styles/index.scss";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
+  rootElement
+);
